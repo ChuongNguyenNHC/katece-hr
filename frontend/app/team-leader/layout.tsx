3018a4a7d9e8f5cc95e27a6a1afefe504b931af5
JohanCorey <nguyenhoangchuong5541@gmail.com>
2025-12-25 22:58:22 +0700

    Push push

--- frontend/app/team-leader/layout.tsx
+++ frontend/app/team-leader/layout.tsx
@@ -8,15 +8,23 @@ export default function TeamLeaderLayout({
   children: React.ReactNode;
 }) {
   return (
-    <RoleGuard allowedRoles={['Tổ trưởng chuyền', 'To truong', 'To truong chuyen']}>
-        <div className="flex min-h-screen bg-slate-50">
-        <Sidebar />
-        <main className="flex-1 pl-64 transition-all duration-300 ease-in-out">
-            <div className="container mx-auto p-8">
-            {children}
-            </div>
-        </main>
+    // <RoleGuard allowedRoles={['Tổ trưởng chuyền', 'To truong', 'To truong chuyen']}>
+    //     <div className="flex min-h-screen bg-slate-50">
+    //     <Sidebar />
+    //     <main className="flex-1 pl-64 transition-all duration-300 ease-in-out">
+    //         <div className="container mx-auto p-8">
+    //         {children}
+    //         </div>
+    //     </main>
+    //     </div>
+    // </RoleGuard>
+    <div className="flex min-h-screen bg-slate-50">
+      <Sidebar />
+      <main className="flex-1 pl-64 transition-all duration-300 ease-in-out">
+        <div className="container mx-auto p-8">
+          {children}
         </div>
-    </RoleGuard>
+      </main>
+    </div>
   );
 }
